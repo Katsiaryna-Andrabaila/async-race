2397d45a93575c806022b47b6006aa32100c1a9c
katsiaryna-andrabaila <androbailoe@gmail.com>
2023-01-29 19:51:21 +0300

    refactor: rewrite actGarage.ts with switch case

--- async-race/src/features/actGarage.ts
+++ async-race/src/features/actGarage.ts
@@ -8,40 +8,45 @@ import createCarListener from './createCarListener';
 import drawNextPage from '../layouts/drawNextPage';
 import drawPreviousPage from '../layouts/drawPreviousPage';
 import deleteCarListener from './deleteCarListener';
+import UI from '../data/UI';
 
 const actGarage = (event: Event) => {
     const { target } = event;
     if (target instanceof HTMLButtonElement) {
         const id = target.closest('.car-item')?.getAttribute('data-id');
-        if (target.classList.contains('create-button')) {
-            createCarListener();
-        }
-        if (target.classList.contains('race-button')) {
-            startRace(target);
-        }
-        if (target.classList.contains('reset-button')) {
-            resetRace(target);
-        }
-        if (target.classList.contains('generate-button')) {
-            generateCars();
-        }
-        if (target.classList.contains('select-button')) {
-            selectCar(Number(id), target);
-        }
-        if (target.classList.contains('remove-button')) {
-            deleteCarListener(Number(id));
-        }
-        if (target.classList.contains('engine-button')) {
-            driveCar(Number(id), target);
-        }
-        if (target.classList.contains('stop-button')) {
-            stopCar(Number(id), target);
-        }
-        if (target.classList.contains('next-page-button')) {
-            drawNextPage(target);
-        }
-        if (target.classList.contains('prev-page-button')) {
-            drawPreviousPage(target);
+        switch (target.textContent) {
+            case UI.createButton:
+                createCarListener();
+                break;
+            case UI.raceButton:
+                startRace(target);
+                break;
+            case UI.resetButton:
+                resetRace(target);
+                break;
+            case UI.generateButton:
+                generateCars();
+                break;
+            case UI.selectButton:
+                selectCar(Number(id), target);
+                break;
+            case UI.removeButton:
+                deleteCarListener(Number(id));
+                break;
+            case UI.engineButton:
+                driveCar(Number(id), target);
+                break;
+            case UI.stopButton:
+                stopCar(Number(id), target);
+                break;
+            case UI.nextButton:
+                drawNextPage(target);
+                break;
+            case UI.prevButton:
+                drawPreviousPage(target);
+                break;
+            default:
+                break;
         }
     }
 };
