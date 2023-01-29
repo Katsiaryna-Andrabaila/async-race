ee6333a3a47ac1d57027511f3dbf6c6a7818cf74
katsiaryna-andrabaila <androbailoe@gmail.com>
2023-01-29 19:34:17 +0300

    fix: remove magic number in driveEngine.ts

--- async-race/src/API/driveEngine.ts
+++ async-race/src/API/driveEngine.ts
@@ -1,11 +1,11 @@
 import { Path } from '../data/types';
-import { BASE_URL } from '../data/constants';
+import { BASE_URL, SUCCESS_DRIVE_CODE } from '../data/constants';
 
 const driveEngine = async (id: number) => {
     const response = await fetch(`${BASE_URL}${Path.engine}?id=${id}&status=drive`, {
         method: 'PATCH',
     }).catch();
-    if (response.status !== 200) {
+    if (response.status !== SUCCESS_DRIVE_CODE) {
         return { success: false };
     }
     return response.json();
--- async-race/src/data/constants.ts
+++ async-race/src/data/constants.ts
@@ -17,3 +17,5 @@ export const ARROWS = {
     up: '↑',
     down: '↓',
 };
+
+export const SUCCESS_DRIVE_CODE = 200;
